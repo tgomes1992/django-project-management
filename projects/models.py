380244c79e814c7e9005c177c9f353a8f7161c67
Thiago Gomes <tt7.thiago@gmail.com>
2021-07-02 00:14:44 -0300

    Adding the projects apps ad projects apps data

---
+++ projects/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Projects(models.Model):
+    startDate = models.DateTimeField(auto_now_add=True)
+    description = models.TextField
+    pass
+
+class Users(models.Model):
+    name = models.CharField(max_length=150)
+    pass
+
